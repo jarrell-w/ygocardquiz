6a09af5c8c687794f8afd6fabfe9e11d1fc2c61b
Jarrell Witherspoon <79674325+jarrell-w@users.noreply.github.com>
2023-08-30 13:25:54 -0400

    Add comments - more refactoring - update readme

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # A Yu-Gi-Oh! Card quiz game created with HTML, CSS, and JAVASCRIPT.
 
-As a side project I decided to create a game based on one of my favourite franchises, Yu-Gi-Oh! The trading card game. The object of the game is to guess the proper card name.
+**A Note:** This project was not made using chatGPT, I try to
+annotate/comment the code in which it's use case may not be immediately apparent in future revisits.
+
+Now, onto the project. As a fun low stakes side project I decided to create a game based on one of my favourite franchises, Yu-Gi-Oh! The trading card game. The object of the game is to guess the proper card name.
 
 **Link to project:** http://ygoquiz.netlify.app/
 
@@ -14,7 +17,12 @@ The current iteration is only from the firsr set as the further in sets you get 
 
 ## Possible Optimizations
 
+May 30, 2022
 I feel a way I could optimize this app is to add comments. Additionally in the future I will add new game modes, which each game mode changing each thing. The website design could also be better.
+
+Update: 08/30/2023
+Refactored some of the code for readability and added ability to play in 3 difficulties instead of just 1.
+Realistically, a large majority of the code would have to be refactored which i see as a good thing!
 
 ## Lessons Learned:
 
--- js/main.js
+++ js/main.js
@@ -2,29 +2,47 @@ document.addEventListener("DOMContentLoaded", getInput);
 
 function getInput() {
   document.querySelector(".select-easy").addEventListener("click", showCard);
-  //document.querySelector(".select-medium").addEventListener("click");
-  //document.querySelector(".select-hard").addEventListener("click");
+  document.querySelector(".select-medium").addEventListener("click", showCard);
+  document.querySelector(".select-hard").addEventListener("click", showCard);
 }
 async function startGame() {
   try {
+    //Try to request api of all cards 
+    //which return a json string
     const response = await fetch(
       `https://db.ygoprodeck.com/api/v7/cardinfo.php`
     );
+    //convert  JSON string to json object
     const data = await response.json();
+
+
     //api structure testObject = { data: [{name: "george"}, {name: "bob"}, {name: "jim"}]}
     //console.log(testObject)
+    //output json object
     return data;
   } catch (e) {
     console.error(e);
   }
 }
 
- async function showCard() {
+ async function showCard(event) {
+  //wait for the api call to finish and return json object
   const cards = await startGame();
-  const card = new cardInfo(cards.data);
+  //grab id of clicked div/level to determine difficulty
+  const difficulty = event.target.id
+  //set difficulty based on a number 1,2, or 3
+  const level = setDifficulty(difficulty)
+  //create new card object from cardInfo class
+  const card = new cardInfo(cards.data, level)
+  //call determine number of cards function of card object 
+  //to select how many cards will be in the pool
+  card.determineNumOfCards() 
+  //set image of card back, show all hidden game elements
+  //show hidden user input, grab user guess 
+  //and match against card name
   const backCardImage =
     "https://i.ibb.co/mv6001m/back-card-yugioh-hd-by-oricacardsbr-dbwk3sn-fullview.jpg";
-  console.log(card.name);
+
   document.querySelectorAll(".hidden").forEach((element) => {
     element.classList.toggle("hidden");
   });
@@ -34,7 +52,10 @@ async function startGame() {
   document.querySelector(".card-desc").innerText = `Description: ${card.desc}`;
   document.querySelector(".card-type").innerText = `Type: ${card.type}`;
   document.querySelector(".card-race").innerText = `Race: ${card.race}`;
+
+
   document.querySelector("#submit").addEventListener("click", () => {
+
     document.querySelector("#card-image").src = `${card.image}`;
     const userGuess = document.querySelector("#input").value;
     document.querySelector(".card-name").innerText = `${card.name}`;
@@ -51,10 +72,46 @@ async function startGame() {
   });
 }
 
+function setDifficulty(difficulty) {
+  if (difficulty = "easy") {
+    return 1
+  } else if (difficult = "medium") {
+    return 2
+  } else {
+    return 3
+  }
+}
+
 class cardInfo {
-  constructor(data) {
+  constructor(data, difficulty) {
+    //grab data difficulty and intialize card info based 
+    //on api data structure of an array of objects.
+    //meaning, in this instance, "this.data" is actually
+    //an array of objects.
     this.data = data;
-    this.randomCard = Math.floor(Math.random() * 9999);;
+    this.difficulty = difficulty
+    this.randomCard = 1
+    this.desc = this.data[this.randomCard].desc;
+    this.name = this.data[this.randomCard].name;
+    this.race = this.data[this.randomCard].race;
+    this.type = this.data[this.randomCard].type;
+    this.image = this.data[this.randomCard].card_images[0].image_url;
+  }
+  //check difficulty number to determine card pool size
+  determineNumOfCards () {
+    if (this.difficulty === 3) {
+      this.randomCard = Math.floor(Math.random() * 10);
+      this.setCardInfo()
+    } else if (this.difficulty === 2) {
+      this.randomCard = Math.floor(Math.random() * 100);
+      this.setCardInfo()
+    } else {
+      this.randomCard = Math.floor(Math.random() * 200);
+      this.setCardInfo()
+    }
+  }
+  //re-set info of card from initial filler info.
+  setCardInfo() {
     this.desc = this.data[this.randomCard].desc;
     this.name = this.data[this.randomCard].name;
     this.race = this.data[this.randomCard].race;
